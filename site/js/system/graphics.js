b7c9848e47e8ffe885787cced22bd7c49267df95
TommyCakes <tomhwarhurst@gmail.com>
2015-07-25 11:36:14 +0100

    Setup Cordinate system

--- site/js/system/graphics.js
+++ site/js/system/graphics.js
@@ -27,6 +27,10 @@ GraphicsSystem.prototype.tick = function() {
       //Clear canvas
       this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+      this.context.save();
+      this.context.translate(this.canvas.width / 2, this.canvas.height);
+      this.context.scale(this.canvas.height, -this.canvas.height);
+
       //Rendering here
   for(var i=0; i<this.entities.length; i++) {
     var entity = this.entities[i]
@@ -36,7 +40,12 @@ GraphicsSystem.prototype.tick = function() {
     }
 
     entity.components.graphics.draw(this.context);
-  }
+}
+
+this.context.restore();
+
+  window.requestAnimationFrame(this.tick.bind(this));
+// };
 };
 
-exports.GraphicsSystem = GraphicsSystem;
+// exports.GraphicsSystem = GraphicsSystem;
